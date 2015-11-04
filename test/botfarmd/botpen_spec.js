2042c6729a435e1bb28a44c85286cb0398a67e46
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2015-11-04 22:59:30 +0200

    Add start/stopAll and loadBots methods to Botpen

--- test/botfarmd/botpen_spec.js
+++ test/botfarmd/botpen_spec.js
@@ -23,10 +23,10 @@ describe('Botpen', function () {
 
   describe('#startAll', function () {
     it('should be an instance method', function () {
-      expect(botpen.startAll).to.be.an('function');
+      expect(botpen.startAll).to.be.a('function');
     });
 
-    it('should invoke #start once each bot in the @roost', function () {
+    it('should invoke #start once on each bot in the @roost', function () {
       botpen.roost = [];
       for (var i = 3; i >= 0; i--) {
         botpen.roost.push({ start: sinon.spy() });
@@ -39,4 +39,33 @@ describe('Botpen', function () {
       });
     });
   });
+
+  describe('#stopAll', function () {
+    it('should be an instance method', function () {
+      expect(botpen.stopAll).to.be.a('function');
+    });
+
+    it('should invoke #stop once on each bot in the @roost', function () {
+      botpen.roost = [];
+      for (var i = 3; i >= 0; i--) {
+        botpen.roost.push({ stop: sinon.spy() });
+      }
+
+      botpen.stopAll();
+
+      botpen.roost.forEach(function (bot) {
+        expect(bot.stop.called).to.equal(true);
+      });
+    });
+  });
+
+  describe('#loadBots', function () {
+    it('should be an instance method', function () {
+      expect(botpen.loadBots).to.be.a('function');
+    });
+
+    xit('should create new Bot instance for each entry provided in botfarmd.json', function () {
+      botpen.loadBots('botfarmd.json');
+    })
+  })
 });
