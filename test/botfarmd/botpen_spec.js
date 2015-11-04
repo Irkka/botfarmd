d7d35cd9842e6d7c811333134ce29f7d55f50fd8
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2015-11-04 09:06:11 +0200

    Define Botpen class

---
+++ test/botfarmd/botpen_spec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var sinon = require('sinon'),
+  sinonChai = require('sinon-chai'),
+  Botpen = require('../../lib/botfarmd/botpen');
+
+describe('Botpen', function () {
+  var botpen;
+
+  beforeEach(function () {
+    botpen = new Botpen();
+  });
+
+  describe('@roost', function() {
+    it('should be an instance property', function () {
+      expect(botpen).to.have.property('roost');
+    });
+
+    it('should be of type array', function() {
+      expect(botpen.roost).to.be.an('array');
+    })
+  });
+
+  describe('#startAll', function () {
+    it('should be an instance method', function () {
+      expect(botpen.startAll).to.be.an('function');
+    });
+
+    it('should invoke #start once each bot in the @roost', function () {
+      botpen.roost = [];
+      for (var i = 3; i >= 0; i--) {
+        botpen.roost.push({ start: sinon.spy() });
+      }
+
+      botpen.startAll();
+
+      botpen.roost.forEach(function (bot) {
+        expect(bot.start.called).to.equal(true);
+      });
+    });
+  });
+});
